60973dd672093a8ec5adc3d20b96ba9e2e28376b
markvstruman <markvstruman@gmail.com>
2026-01-20 04:00:53 -0500

    UPDATES!

--- CraneClubNetscape/src/app/sidebar/sidebar.ts
+++ CraneClubNetscape/src/app/sidebar/sidebar.ts
@@ -1,4 +1,8 @@
+import { OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
+import Webamp from 'webamp';
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -6,6 +10,43 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {
+export class Sidebar implements AfterViewInit {
+  isOpen: boolean = false;
+  ngAfterViewInit(): void {
+    this.initializeWebamp();
+  }
 
+  initializeWebamp(): void {
+    const webamp = new Webamp({
+      initialTracks: [
+        {
+          metaData: {
+            artist: "Aphex Twin",
+            title: "Xtal"
+          },
+          // NOTE: Your audio file must be served from the same domain as your HTML
+          // file, or served with permissive CORS HTTP headers:
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+          url: "https://raw.githubusercontent.com/AmooshN/cranecult.github.io/main/Music/Xtal.mp3",
+          duration: 4.54
+        },
+        {
+          metaData: {
+            artist: "George Clanton",
+            title: "Livin' Loose"
+          },
+          url: "https://raw.githubusercontent.com/AmooshN/cranecult.github.io/main/Music/Livin_Loose.mp3",
+          duration: 5.50
+        }
+      ],
+    });
+
+    // Returns a promise indicating when it's done loading.
+    const webampContainer = document.getElementById('webamp');
+    webamp.renderWhenReady(webampContainer!);
+  }
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
 }
